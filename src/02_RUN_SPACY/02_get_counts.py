c432bfc1697bd1a65fbb7b657e8ed2c5e7915c37
cademccu <cade21mccumber@gmail.com>
2025-10-20 16:56:08 -0600

    the long awaited stats aggregation

--- src/02_RUN_SPACY/02_get_counts.py
+++ src/02_RUN_SPACY/02_get_counts.py
@@ -1,13 +1,18 @@
+"""
+This file just does the TOTAL counts -- nothing level or collaborative
+based
+"""
 
 import sys  
 sys.path.append("..") # add the correct path
 
 import subprocess
 import os
+import re
 from CONFIGURATION import PATH_TO_CCOT, PATH_TO_DATA
 
 
-output_file_path    = "../03_REPORTS/TOTAL_COUNTS.txt"
+output_file_path    = "../03_REPORTS/TOTAL_COUNTS_ALL.txt"
 path_to_work_output = os.path.join(PATH_TO_DATA, "WORK_OUTPUT")
 
 fout = open(output_file_path, "wt") 
@@ -20,6 +25,8 @@ fout.write("\n\n\n")
 
 
 files = subprocess.check_output(["ls", "-1", os.path.join(PATH_TO_DATA, "WORK_OUTPUT")]).decode("utf-8").split("\n")    
+
+
 
 # just going to hold all the information in this here dictionary
 total_counts = {}
@@ -37,6 +44,11 @@ for fname in files:
         line = f.readline() # burn the filename value
         while line.startswith("=== "):
             line = line[4:].split("|")
+
+            if re.match(r"FORMULAIC\sCOUNT\s[AB]{1}[0-9]{3}", line[0]):
+                # we don't want the individual formulaic counts here
+                line = f.readline()
+                continue
             if line[0].strip() not in total_counts.keys():
                 total_counts[line[0].strip()] = 0
             total_counts[line[0].strip()] += int(line[1].strip())
--- src/03_REPORTS/README.txt
+++ src/03_REPORTS/README.txt
@@ -1,3 +1,4 @@
 
 
-These are output reports of the program.
+These are output reports of the program, and some of the code that generates them.
+
